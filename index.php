f1c45229699950b823f1b83c2be07f18bc54d9c5
jbrou31 <justinfb@gmail.com>
2015-02-22 19:41:09 -0600

    add index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <p>
+            The Iterators are coming!
+        </p>
+    </body>
+</html>
