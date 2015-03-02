e2350a74642b868456c965123ae2796f3974f323
jbrou31 <justinfb@gmail.com>
2015-03-02 15:13:43 -0600

    add image background to jumbotron; fix menu when browsing on a mobile device

--- index.php
+++ index.php
@@ -9,6 +9,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    .jumbotron {
+      color: white;
+      height: 500px;
+      width: 100%;
+      background-size: cover; 
+      background-position: center; 
+      background: url('img/calendar_1600.jpg') no-repeat;
+    }
+  </style>
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -19,51 +29,62 @@
 </head>
 
 <body>
-<div class="container-fluid">
-  <div class="row-fluid">
-    <div class="col-md-12 column">
-      <nav class="navbar navbar-default navbar-inverse" role="navigation">
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu<strong class="caret"></strong></a>
-               <ul class="dropdown-menu">
-                 <li>
-                   <a href="#">Open Calendar</a>
-                 </li>
-                 <li>
-                   <a href="#">Do something else</a>
-                 </li>
-              </ul>
-            </li>
-          </ul>
-          <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-              <input type="text" class="form-control" />
-            </div> <button type="submit" class="btn btn-default">Search Tasks</button>
-          </form>
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <a href="login.php">Sign In</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <div class="jumbotron">
-        <h1>
-          4get-me-not
-        </h1>
-      <div>
-        Your go to task/event organizer. Put me in your pocket.
-      </div><br>
-      <p>
-        <a class="btn btn-primary btn-large" href="#">Learn more</a>
-      </p>
-      </div>
+<header class="navbar navbar-default navbar-inverse navbar-static-top" id="top" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"> Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    <a class="navbar-brand" href="#">4get-me-not</a>
     </div>
+    <nav class="collapse navbar-collapse navbar-inverse">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu<strong class="caret"></strong></a>
+          <ul class="dropdown-menu">
+            <li>
+              <a href="#">Open Calendar</a>
+            </li>
+            <li>
+              <a href="#">Do something else</a>
+            </li>
+          </ul>
+         </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" />
+        </div> <button type="submit" class="btn btn-default">Search Tasks</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a href="login.php">Sign In</a>
+        </li>
+      </ul>
+    </nav>
   </div>
-  <div class="row-fluid">
-    <div class="col-md-4 column">
+</header>
+
+<div class="jumbotron vertical-center">
+  <div class="container">
+    <h1>
+      4get-me-not
+    </h1>
+    <div>
+      Your go to task/event organizer. Put me in your pocket.
+    </div><br>
+    <p>
+      <a class="btn btn-primary btn-large" href="#">Learn more</a>
+    </p>  
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
       <h2>
        Task Reminders
       </h2>
@@ -73,8 +94,10 @@
        <p>
         <a class="btn btn-primary" href="#">View details</a>
        </p>
-     </div>
-     <div class="col-md-4 column">
+    </div>
+<!--  </div>
+  <div class="row"> -->
+    <div class="col-md-4">
       <h2>
        Event Notifications
       </h2>
@@ -85,7 +108,9 @@
         <a class="btn btn-primary" href="#">View details</a>
       </p>
     </div>
-    <div class="col-md-4 column">
+<!--  </div>
+  <div class="row"> -->
+    <div class="col-md-4">
       <h2>
         QR Code Scanning
       </h2>
@@ -96,6 +121,9 @@
         <a class="btn btn-primary" href="#">View details</a>
       </p>
     </div>
+<!--    <div class="col-md-3 column">
+      <img src="img/qrcode.jpg" class="img-responsive" alt="QR image">
+    </div> -->
   </div>
 </div>
 
