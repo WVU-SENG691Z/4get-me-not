27b83252d1f8dcf1257b71c497cc096242d73490
jbrou31 <justinfb@gmail.com>
2015-03-17 10:21:03 -0500

    move header to common file to avoid copypasta across many files, isolates changes to a single place

---
+++ header.php
@@ -0,0 +1,47 @@
+<header class="navbar navbar-default navbar-fixed-top" id="top" role="banner"
+        style="margin-bottom: -20px;">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"> Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <div class="navbar-brand">4get-me-not</div>
+    </div>
+    <nav class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu<strong class="caret"></strong></a>
+          <ul class="dropdown-menu">
+            <li>
+            <li>
+              <a href="calendar.php">Calendar</a>
+            </li>
+          </ul>
+         </li>
+         <li>
+           <a href="#">About</a>
+         </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" />
+        </div> <button type="submit" class="btn btn-default">Search Tasks</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <div class="btn-toolbar">
+            <button id="signin" style="background-color: #1E90FF; color: white"
+                    class="navbar-btn btn btn-default">Sign In</button>
+            <button id="createaccount" style="background-color: #D3D3D3;"
+                    class="navbar-btn btn btn-default">Create Account</button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+
+
