448b45543c586d3fbd9b2f7a6735dd3bed3a9773
Mark Ziecina <ziecina@gmail.com>
2015-04-17 08:30:56 +0400

    Started code to make QR code tag the events.

--- handleQRScan.php
+++ handleQRScan.php
@@ -1,20 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
 
-Need a form with the event name prefilled
-<form>
-<input name="EventName" type="text" disabled="true" value="Changed Air Filter"/><br />
-<input name="TimeAdjustment" type="radio" value"Now">Now
-<input name="TimeAdjustment" type="radio" value"-30">30 Minutes Ago
-<input name="TimeAdjustment" type="radio" value"-60">1 Hour Ago
-<input name="TimeAdjustment" type="radio" value"-120">2 Hour Ago<br />
-<input type="submit" value="Add Event"/>
+<head>
+  <title>4get-me-not</title>
+
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="css/bootstrap.datetimepicker.css" rel="stylesheet" media="screen"/>
+  <link href="css/smallmodal.css" rel="stylesheet" media="screen"/>
+
+  <style>
+    .alert-sm
+    {
+        padding-top: 6px;
+        padding-bottom: 6px;
+    }
+  </style>
+
+</head>
+
+<body>
+
+<?php
+$dbLink = pg_connect("host=127.0.0.1 dbname=dev1 user=postgres") or die("Unable to connect to database");
+$query  = "SELECT * FROM handles WHERE handle_id='". $_GET['event_id'] ."';";
+$result = pg_query($dbLink, $query);
+$row = pg_fetch_object($result)
+?>
+
+<form action="tagEvent.php" method="post">
+	<input name="EventName" type="text" disabled="true" value="<?php echo $row->title; ?>"/><br />
+	<input name="handle_id" type="hidden" value="<?php echo $row->handle_id; ?>">
+	<input name="TimeAdjustment" type="radio" value"Now">Now
+	<input name="TimeAdjustment" type="radio" value"-30">30 Minutes Ago
+	<input name="TimeAdjustment" type="radio" value"-60">1 Hour Ago
+	<input name="TimeAdjustment" type="radio" value"-120">2 Hour Ago<br />
+	<input type="submit" value="Add Event"/>
 </form>
 
 
 Need a list of all events with the same event_handle_id
-<ul>2014-12-03</ul>
-<ul>2015-02-05</ul>
-<ul>2015-03-20</ul>
+<?php 
+    $dbLink = pg_connect("host=127.0.0.1 dbname=dev1 user=postgres") or die("Unable to connect to database");
+	$query_events  = "SELECT * FROM events WHERE handle_id='". $_GET['event_id'] ."'";
+	$result_events = pg_query($dbLink, $query_events);
+
+	while ($events = pg_fetch_object($result_events)): ?>
+			<p><?php echo $events->event_time_start; ?> - <?php echo $events->event_time_end; ?></p>
+<?php endwhile; ?>
 
 
+</body>
+<script src="js/jquery-1.11.2.min.js"></script>
+<script src="js/moment.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="js/bootstrap.datetimepicker.min.js"></script>
+<script src="js/signin.js"></script>
+<script src="js/jquery.validate.min.js"></script>
 
 
---
+++ tagEvent.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+    header('Content-type: application/json');
+
+   if(true)
+    {
+        $responseArray = array();
+
+        $dbLink = pg_connect("host=127.0.0.1 dbname=dev1 user=postgres") 
+                    or die("Unable to connect to database");
+
+        $userid = $_SESSION['USER_ID'];
+		$handle_id = $_REQUEST['handle_id'];
+        $eventStart = $_REQUEST['TimeAdjustment'];
+		$eventEnd = $eventStart; 
+		
+        $result = 
+            pg_query_params($dbLink, 'INSERT INTO events (userid, handle_id, event_time_start, event_time_end) VALUES($1, $2, NOW() - INTERVAL \''.$3.' minutes\', NOW() - INTERVAL \''.$4.' minutes\')', 
+                            array($userid, $handle_id, $eventStart, $eventEnd));
+
+        if(!$result) 
+        {
+            $responseArray['status'] = 'error';
+            $responseArray['data'] = '<div class="alert alert-danger alert-sm" role="alert">'.
+                                     'Coult not add your event.'.pg_last_error($dbLink).
+                                     '</div>';
+        }
+        else
+        {
+            $responseArray['status'] = 'success';
+            $responseArray['data'] = '<div class="alert alert-success alert-sm" role="alert">'.
+                                     'Successfully added your new event!</div>';
+        }
+
+        echo json_encode($responseArray);
+    }
+?>
